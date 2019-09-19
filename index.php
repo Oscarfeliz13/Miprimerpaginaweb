92d7fbec9dfd87f23b12c05c97b9e878e95c7f89
Oscarfeliz13 <oscarfeliz_08@hotmail.com>
2019-09-19 22:00:46 +0200

    Primera pagina
    Mi primera pagina web

---
+++ index.php
@@ -0,0 +1,13 @@
+<html> 
+   <head> 
+       <title>test 001</title>
+	   </head>
+        <body>	
+<?php
+$var1 = "<h1>HOLA SOY OSCAR FELIZ<h1>";
+echo $var1;
+$var1 = "<p>MI HOBBY SON: SOFTBALL,BASKETBALL,AUTOS<p>";
+echo $var1;
+?> 
+</body>		
+</html> 
